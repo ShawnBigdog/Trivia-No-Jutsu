5786cc1b55b11c13412801290d62ed8b2a2fb556
Shawn Jackson <140444687+ShawnBigdog@users.noreply.github.com>
2025-04-07 23:45:25 +0000

    gh4reun

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
 function flipTile(tile) {
-    const tileInner = tile.querySelector('.tile-inner');
-    console.log(tileInner, "has been clicked");
+    const tileInner = tile.querySelector(".tile-inner");
     // Toggle the flip effect when the tile is clicked
-    tileInner.style.transform = tileInner.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
-}
-function checkEnter(event) {
-    if (event.key === "Enter") {
-        answer();
-    }
-}
-
-function answer() {
-    let correctAnswer = "Kushina Uzumaki"; // Correct answer
-    let userAnswer = document.getElementById("answer").value.trim();
-
-    if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
-        alert("Correct! Naruto's mother is Kushina Uzumaki.");
-    } else {
-        alert("Incorrect. Try again!");
-    }
-}
+    tileInner.style.transform =
+      tileInner.style.transform === "rotateY(180deg)"
+        ? "rotateY(0deg)"
+        : "rotateY(180deg)";
+  }
+  
