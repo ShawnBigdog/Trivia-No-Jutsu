a51a6de67672c9b806b20a02e3570427681ca13a
Shawn Jackson <140444687+ShawnBigdog@users.noreply.github.com>
2025-04-09 23:37:45 +0000

    fhrvbh

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 function flipTile(tile) {
     const tileInner = tile.querySelector(".tile-inner");
+
     // Toggle the flip effect when the tile is clicked
     tileInner.style.transform =
       tileInner.style.transform === "rotateY(180deg)"
         ? "rotateY(0deg)"
         : "rotateY(180deg)";
-  }
+        
+        } 
+  
+  /*const tile = document.querySelector('.tile');
+  let flipped = false;
+
+  tile.addEventListener ('click', () => {
+    if(!flipped){
+      tile.classList.add('flipped');
+      flipped = true;
+    }
+  });*/
+  var uInp = document.getElementById("answer");
+
+function answer() {
+  if (uInp.value == "ok") {
+    alert("correct");
+  }else{
+    alert("incorrect");
+  };
+};
+
+document.querySelector('input[type="submit"]').addEventListener('click', answer);
   function func1(event) {
     
     if (document.getElementById("check").checked) {
       event.stopPropagation();
     }
   }
-  const tile = document.querySelector('.tile');
-  let flipped = false;
-
-  tile.addEventListener('click',()=>{
-    if(!flipped){
-      tile.classList.add('flipped');
-      flipped = true;
-    }
-  });
+  
