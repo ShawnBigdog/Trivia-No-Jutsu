8a6c01a84c64a4bf0917bf9c74e8b2b65e342713
Shawn Jackson <140444687+ShawnBigdog@users.noreply.github.com>
2025-04-24 00:02:24 +0000

    wednesday april 23

--- script.js
+++ script.js
@@ -34,16 +34,16 @@ function func1(event) {
     event.stopPropagation();
   }*/
 
-  function flipTile(tile) {
-    const tileInner = tile.querySelector(".tile-inner");
-    // Toggle the flip effect when the tile is clicked
-    tileInner.style.transform =
-      tileInner.style.transform === "rotateY(180deg)"
-        ? "rotateY(0deg)"
-        : "rotateY(180deg)";
-  }
-  function func1(event) {
-    if (document.getElementById("check").checked) {
-      event.stopPropagation();
-    }
-  }
+  // function flipTile(tile) {
+  //   const tileInner = tile.querySelector(".tile-inner");
+  //   // Toggle the flip effect when the tile is clicked
+  //   tileInner.style.transform =
+  //     tileInner.style.transform === "rotateY(180deg)"
+  //       ? "rotateY(0deg)"
+  //       : "rotateY(180deg)";
+  // }
+  // function func1(event) {
+  //   if (document.getElementById("check").checked) {
+  //     event.stopPropagation();
+  //   }
+  // }
